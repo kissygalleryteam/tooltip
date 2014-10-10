e02dc638f8f4a3b429c3b25535238223877303e2
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-10 14:12:02 +0800

    sync daily version 2.1.0

---
+++ 2.1.0/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-10-10 2:09:56 PM*/
+KISSY.config("modules",{"kg/tooltip/2.1.0/index":{requires:["node"]}});
---
+++ 2.1.0/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/tooltip/2.1.0/index': { requires: ['node']} 
+});
